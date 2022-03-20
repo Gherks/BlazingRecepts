81e09a1888aafee0d73a2c754098eb339c0dc3a0
Gherks <nils-joakim.andersson@hotmail.com>
2022-03-20 20:00:38 +0100

    Fix issue with ingredient category relationship with new ingredient

--- Server/Repositories/IngredientsRepository.cs
+++ Server/Repositories/IngredientsRepository.cs
@@ -1,14 +1,18 @@
 ﻿using BlazingRecept.Server.Context;
 using BlazingRecept.Server.Entities;
 using BlazingRecept.Server.Repositories.Interfaces;
+using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlazingRecept.Server.Repositories;
 
 public class IngredientsRepository : RepositoryBase<Ingredient>, IIngredientsRepository
 {
-    public IngredientsRepository(BlazingReceptContext dbContext) : base(dbContext)
+    private IIngredientCategoryRepository? _ingredientCategoryRepository;
+
+    public IngredientsRepository(BlazingReceptContext context, IIngredientCategoryRepository ingredientCategoryRepository) : base(context)
     {
+        _ingredientCategoryRepository = ingredientCategoryRepository;
     }
 
     public async Task<bool> AnyAsync(string name)
@@ -21,6 +25,29 @@ public class IngredientsRepository : RepositoryBase<Ingredient>, IIngredientsRep
         {
             return false;
         }
+    }
+
+    public override async Task<Ingredient> AddAsync(Ingredient ingredient)
+    {
+        if (_ingredientCategoryRepository == null) throw new InvalidOperationException("Ingredient category repository can not be used because it has not been set.");
+
+        try
+        {
+            IngredientCategory? ingredientCategory = await _ingredientCategoryRepository.GetByIdAsync(ingredient.IngredientCategoryId);
+
+            if (ingredientCategory == null) throw new InvalidOperationException("Ingredient category can not be used because it could not be fetched from database.");
+
+            ingredient.IngredientCategory = ingredientCategory;
+            _context.Ingredient.Add(ingredient);
+
+            await _context.SaveChangesAsync();
+            await _context.Entry(ingredient).ReloadAsync();
+        }
+        catch (Exception)
+        {
+        }
+
+        return ingredient;
     }
 }
  
