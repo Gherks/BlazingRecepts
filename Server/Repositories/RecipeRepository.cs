317ecb858e29b6a303a0e491203feda0cba8b424
Gherks <nils-joakim.andersson@hotmail.com>
2022-03-27 00:04:02 +0100

    Add overridden AddAsync call in recipe repository

--- Server/Repositories/RecipeRepository.cs
+++ Server/Repositories/RecipeRepository.cs
@@ -7,8 +7,11 @@ namespace BlazingRecept.Server.Repositories;
 
 public class RecipeRepository : RepositoryBase<Recipe>, IRecipeRepository
 {
-    public RecipeRepository(BlazingReceptContext context) : base(context)
+    private IIngredientCategoryRepository? _ingredientCategoryRepository;
+
+    public RecipeRepository(BlazingReceptContext context, IIngredientCategoryRepository ingredientCategoryRepository) : base(context)
     {
+        _ingredientCategoryRepository = ingredientCategoryRepository;
     }
 
     public async Task<bool> AnyAsync(string name)
@@ -21,5 +24,28 @@ public class RecipeRepository : RepositoryBase<Recipe>, IRecipeRepository
         {
             return false;
         }
+    }
+
+    public override async Task<Recipe> AddAsync(Recipe recipe)
+    {
+        if (_ingredientCategoryRepository == null) throw new InvalidOperationException("Ingredient category repository cannot be used because it has not been set.");
+
+        try
+        {
+            //IngredientCategory? ingredientCategory = await _ingredientCategoryRepository.GetByIdAsync(ingredient.IngredientCategoryId);
+
+            //if (ingredientCategory == null) throw new InvalidOperationException("Ingredient category cannot be used because it could not be fetched from database.");
+
+            //ingredient.IngredientCategory = ingredientCategory;
+            //_context.Ingredient.Add(ingredient);
+
+            await _context.SaveChangesAsync();
+            await _context.Entry(recipe).ReloadAsync();
+        }
+        catch (Exception)
+        {
+        }
+
+        return recipe;
     }
 }
