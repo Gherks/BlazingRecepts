61fbfa5a2220ecd83b122527a964966edb57e7b7
Gherks <nils-joakim.andersson@hotmail.com>
2022-03-27 01:00:13 +0100

    Add initial design for index page
    Page will list all recipes

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -1,7 +1,30 @@
+using BlazingRecept.Client.Services.Interfaces;
+using BlazingRecept.Shared.Dto;
 using Microsoft.AspNetCore.Components;
 
 namespace BlazingRecept.Client.Pages;
 
 public partial class Index : ComponentBase
 {
+    public IReadOnlyList<RecipeDto>? _recipes = new List<RecipeDto>();
+
+    [Inject]
+    public IRecipeService? RecipeService { get; set; }
+
+    [Inject]
+    public NavigationManager? NavigationManager { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (RecipeService == null) throw new InvalidOperationException();
+
+        _recipes = await RecipeService.GetAllAsync();
+    }
+
+    private void HandleRecipeNavigation(RecipeDto recipeDto)
+    {
+        if (NavigationManager == null) throw new InvalidOperationException();
+
+        NavigationManager.NavigateTo($"recipes/{recipeDto.Id}");
+    }
 }
