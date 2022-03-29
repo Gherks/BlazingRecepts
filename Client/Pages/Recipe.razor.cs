45ef715c23c7c574508307c1726bd7c64801ddea
Gherks <nils-joakim.andersson@hotmail.com>
2022-03-29 21:27:59 +0200

    Add new information to recipe page

--- Client/Pages/Recipe.razor.cs
+++ Client/Pages/Recipe.razor.cs
@@ -35,4 +35,92 @@ public partial class Recipe : ComponentBase
 
         return ingredientMeasurementDto.Measurement.Trim() + " " + ingredientMeasurementDto.MeasurementUnit.ToSymbol();
     }
+    private int GetTotalGrams()
+    {
+        if (_recipeDto == null) throw new InvalidOperationException();
+
+        return _recipeDto.IngredientMeasurementDtos.Sum(ingredientMeasurementDto => ingredientMeasurementDto.Grams);
+    }
+
+    private double GetTotalFat()
+    {
+        if (_recipeDto == null) throw new InvalidOperationException();
+
+        double totalFat = 0.0;
+
+        foreach (IngredientMeasurementDto ingredientMeasurementDto in _recipeDto.IngredientMeasurementDtos)
+        {
+            totalFat += ingredientMeasurementDto.IngredientDto.Fat * ingredientMeasurementDto.IngredientDto.Fat;
+        }
+
+        return totalFat;
+    }
+
+    private double GetTotalCarbohydrates()
+    {
+        if (_recipeDto == null) throw new InvalidOperationException();
+
+        double totalCarbohydrates = 0.0;
+
+        foreach (IngredientMeasurementDto ingredientMeasurementDto in _recipeDto.IngredientMeasurementDtos)
+        {
+            totalCarbohydrates += ingredientMeasurementDto.IngredientDto.Carbohydrates * ingredientMeasurementDto.IngredientDto.Carbohydrates;
+        }
+
+        return totalCarbohydrates;
+    }
+
+    private double GetTotalProtein()
+    {
+        if (_recipeDto == null) throw new InvalidOperationException();
+
+        double totalProtein = 0.0;
+
+        foreach (IngredientMeasurementDto ingredientMeasurementDto in _recipeDto.IngredientMeasurementDtos)
+        {
+            totalProtein += ingredientMeasurementDto.IngredientDto.Protein * ingredientMeasurementDto.IngredientDto.Protein;
+        }
+
+        return totalProtein;
+    }
+
+    private double GetTotalCalories()
+    {
+        if (_recipeDto == null) throw new InvalidOperationException();
+
+        double totalCalories = 0.0;
+
+        foreach (IngredientMeasurementDto ingredientMeasurementDto in _recipeDto.IngredientMeasurementDtos)
+        {
+            totalCalories += ingredientMeasurementDto.IngredientDto.Calories * ingredientMeasurementDto.IngredientDto.Calories;
+        }
+
+        return totalCalories;
+    }
+
+    private double GetGramsPerPortion()
+    {
+        if (_recipeDto == null) throw new InvalidOperationException();
+
+        return Convert.ToDouble(GetTotalGrams()) / _recipeDto.PortionAmount;
+    }
+
+    private double GetProteinPerPortion()
+    {
+        if (_recipeDto == null) throw new InvalidOperationException();
+
+        return Convert.ToDouble(GetTotalProtein()) / _recipeDto.PortionAmount;
+    }
+
+    private double GetCaloriesPerPortion()
+    {
+        if (_recipeDto == null) throw new InvalidOperationException();
+
+        return Convert.ToDouble(GetTotalCalories()) / _recipeDto.PortionAmount;
+    }
+
+    private double GetProteinPerCalorie()
+    {
+        return GetTotalProtein() / GetTotalCalories();
+    }
 }
