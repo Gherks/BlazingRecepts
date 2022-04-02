cc712d1e25424907aefe4fbef2f256fae1ad698d
Gherks <nils-joakim.andersson@hotmail.com>
2022-04-02 21:57:59 +0200

    Integrate RecipeCategoryTable into Index page

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -6,25 +6,4 @@ namespace BlazingRecept.Client.Pages;
 
 public partial class Index : ComponentBase
 {
-    private IReadOnlyList<RecipeDto>? _recipeDtos = new List<RecipeDto>();
-
-    [Inject]
-    public IRecipeService? RecipeService { get; set; }
-
-    [Inject]
-    public NavigationManager? NavigationManager { get; set; }
-
-    protected override async Task OnInitializedAsync()
-    {
-        if (RecipeService == null) throw new InvalidOperationException();
-
-        _recipeDtos = await RecipeService.GetAllAsync();
-    }
-
-    private void HandleRecipeNavigation(RecipeDto recipeDto)
-    {
-        if (NavigationManager == null) throw new InvalidOperationException();
-
-        NavigationManager.NavigateTo($"recipe/{recipeDto.Id}");
-    }
 }
