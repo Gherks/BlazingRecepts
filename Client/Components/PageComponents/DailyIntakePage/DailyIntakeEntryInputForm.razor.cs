c4e49bac478a6d210139b40c0757bac2296e02b2
Gherks <nils-joakim.andersson@hotmail.com>
2022-04-20 22:05:33 +0200

    Add daily intake entry page components

---
+++ Client/Components/PageComponents/DailyIntakePage/DailyIntakeEntryInputForm.razor.cs
@@ -0,0 +1,190 @@
+using BlazingRecept.Client.Components.PageComponents.Base;
+using BlazingRecept.Client.Extensions;
+using BlazingRecept.Client.Pages;
+using BlazingRecept.Client.Services.Interfaces;
+using BlazingRecept.Client.Utilities;
+using BlazingRecept.Shared.Dto;
+using Blazored.Toast.Services;
+using Microsoft.AspNetCore.Components;
+using Serilog;
+
+namespace BlazingRecept.Client.Components.PageComponents.DailyIntakePage;
+
+public partial class DailyIntakeEntryInputForm : PageComponentBase
+{
+    private static readonly string _logProperty = "Domain";
+    private static readonly string _logDomainName = "DailyIntakeEntryInputForm";
+
+    private Form _form = new();
+
+    private CustomValidation? _customValidation;
+
+    [CascadingParameter]
+    protected internal DailyIntake? DailyIntakePage { get; private set; }
+
+    [Parameter]
+    public Guid CollectionId { get; set; }
+
+    [Inject]
+    public IDailyIntakeEntryService? DailyIntakeEntryService { get; set; }
+
+    [Inject]
+    public IToastService? ToastService { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        IsLoading = true;
+
+        await base.OnInitializedAsync();
+
+        IsLoading = false;
+    }
+
+    private async Task<IEnumerable<string>> SearchForRecipeOrIngredient(string searchTerm)
+    {
+        if (DailyIntakePage == null)
+        {
+            string errorMessage = "DailyIntake page reference has not been set during ingredient input search procedure.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        if (DailyIntakePage.Ingredients == null)
+        {
+            string errorMessage = "DailyIntake page reference contains no ingredients during daily intake ingredient input search procedure.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        if (DailyIntakePage.Recipes == null)
+        {
+            string errorMessage = "DailyIntake page reference contains no recipes during daily intake recipe input search procedure.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        List<IngredientDto> foundIngredients = DailyIntakePage.Ingredients.Where(ingredientDto => ingredientDto.Name.ToLower().Contains(searchTerm.ToLower())).ToList();
+        List<RecipeDto> foundRecipes = DailyIntakePage.Recipes.Where(recipeDto => recipeDto.Name.ToLower().Contains(searchTerm.ToLower())).ToList();
+
+        List<string> foundProductNames = new();
+
+        foreach (IngredientDto ingredientDto in foundIngredients)
+        {
+            foundProductNames.Add(ingredientDto.Name);
+        }
+
+        foreach (RecipeDto recipeDto in foundRecipes)
+        {
+            foundProductNames.Add(recipeDto.Name);
+        }
+
+        if (foundProductNames.Count == 1)
+        {
+            _form.ProductName = foundProductNames.First();
+        }
+
+        return await Task.FromResult(foundProductNames);
+    }
+
+    private async Task HandleValidFormSubmitted()
+    {
+        if (DailyIntakePage == null)
+        {
+            string errorMessage = "Daily intake page reference is not available during form validation.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        if (DailyIntakeEntryService == null)
+        {
+            string errorMessage = "Daily intake entry service is not available during form validation.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        if (ToastService == null)
+        {
+            string errorMessage = "Toast service is not available during form validation.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        if (Validate())
+        {
+            DailyIntakeEntryDto newDailyIntakeEntryDto = CreateDailyIntakeEntryDtoFromForm();
+            DailyIntakeEntryDto? savedDailyIntakeEntryDto = await DailyIntakeEntryService.SaveAsync(newDailyIntakeEntryDto);
+
+            if (savedDailyIntakeEntryDto != null && savedDailyIntakeEntryDto.Id != Guid.Empty)
+            {
+                DailyIntakePage.UpsertDailyIntakeEntryIntoCollection(savedDailyIntakeEntryDto);
+
+                _form = new();
+                ToastService.ShowSuccess("Post för dagligt intag tillagd!");
+
+                StateHasChanged();
+            }
+            else
+            {
+                ToastService.ShowError("Kunde ej lägga till post för dagligt intag.");
+            }
+        }
+    }
+
+    private bool Validate()
+    {
+        if (_customValidation == null)
+        {
+            string errorMessage = "Custom validation object is not available during validation.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        _customValidation.ClearErrors();
+
+        Dictionary<string, List<string>> errors = new();
+
+        if (string.IsNullOrWhiteSpace(_form.ProductName))
+        {
+            errors.Add(nameof(_form.ProductName), new List<string>() {
+                "Namn måste anges."
+            });
+        }
+
+        InputValidation.ValidateStringToDouble(_form.Amount, nameof(_form.Amount), "Mängd", errors);
+
+        if (errors.Count > 0)
+        {
+            _customValidation.DisplayErrors(errors);
+            return false;
+        }
+
+        return true;
+    }
+
+    private DailyIntakeEntryDto CreateDailyIntakeEntryDtoFromForm()
+    {
+        if (DailyIntakePage == null)
+        {
+            string errorMessage = "Daily intake page reference is not available during daily intake entry construction.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        DailyIntakeEntryDto dailyIntakeEntryDto = new()
+        {
+            ProductName = _form.ProductName.Trim(),
+            Amount = double.Parse(_form.Amount),
+            CollectionId = CollectionId
+        };
+
+        dailyIntakeEntryDto.LoadFromProductListsByName(DailyIntakePage.Ingredients, DailyIntakePage.Recipes);
+
+        return dailyIntakeEntryDto;
+    }
+
+    private class Form
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public string Amount { get; set; } = string.Empty;
+    }
+}
---
+++ Client/Components/PageComponents/DailyIntakePage/DailyIntakeTable.razor.cs
@@ -0,0 +1,295 @@
+using BlazingRecept.Client.Components.PageComponents.Base;
+using BlazingRecept.Client.Components.Utilities;
+using BlazingRecept.Client.Extensions;
+using BlazingRecept.Client.Pages;
+using BlazingRecept.Client.Services.Interfaces;
+using BlazingRecept.Shared.Dto;
+using Blazored.Toast.Services;
+using Microsoft.AspNetCore.Components;
+using Serilog;
+
+namespace BlazingRecept.Client.Components.PageComponents.DailyIntakePage;
+
+public partial class DailyIntakeTable : PageComponentBase
+{
+    private static readonly string _logProperty = "Domain";
+    private static readonly string _logDomainName = "DailyIntakeTable";
+
+    private Guid _editingDailyIntakeEntryGuid = Guid.Empty;
+
+    private RemovalConfirmationModal<DailyIntakeEntryDto>? _removalConfirmationModal;
+
+    [CascadingParameter]
+    protected internal DailyIntake? DailyIntakePage { get; private set; }
+
+    [Inject]
+    public IDailyIntakeEntryService? DailyIntakeService { get; set; }
+
+    [Inject]
+    public IIngredientService? IngredientService { get; set; }
+
+    [Inject]
+    public IRecipeService? RecipeService { get; set; }
+
+    [Inject]
+    public IToastService? ToastService { get; set; }
+
+    private void HandleDailyIntakeEntryEditClicked(Guid editingDailyIntakeEntryGuid)
+    {
+        _editingDailyIntakeEntryGuid = editingDailyIntakeEntryGuid;
+        StateHasChanged();
+    }
+
+    private void HandleDailyIntakeEntryRemovalModalOpen(DailyIntakeEntryDto? dailyIntakeEntryDto)
+    {
+        if (_removalConfirmationModal == null)
+        {
+            string errorMessage = "Confirmation modal cannot be opened because it has not been set.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        if (dailyIntakeEntryDto == null)
+        {
+            string errorMessage = "Cannot start daily intake entry removal process because daily intake entry has not been set.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new ArgumentNullException(nameof(dailyIntakeEntryDto), errorMessage);
+        }
+
+        _removalConfirmationModal.Open(dailyIntakeEntryDto, "Ta bort post för dagligt intag", dailyIntakeEntryDto.ProductName);
+    }
+
+    private async Task HandleDailyIntakeEntryEditConfirmed(DailyIntakeEntryDto dailyIntakeEntryDto)
+    {
+        if (DailyIntakePage == null)
+        {
+            string errorMessage = "Cannot save edited daily intake entry because the daily intake page reference has not been set.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        if (DailyIntakeService == null)
+        {
+            string errorMessage = "Cannot save edited daily intake entry because the daily intake service has not been set.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        if (IngredientService == null)
+        {
+            string errorMessage = "Cannot save edited daily intake entry because the ingredient service has not been set.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        if (RecipeService == null)
+        {
+            string errorMessage = "Cannot save edited daily intake entry because the recipe service has not been set.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        DailyIntakeEntryDto? savedDailyIntakeEntryDto = await DailyIntakeService.SaveAsync(dailyIntakeEntryDto);
+
+        if (savedDailyIntakeEntryDto == null)
+        {
+            string errorMessage = "Something went wrong when saving an edited daily intake entry.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        await savedDailyIntakeEntryDto.LoadFromProductServices(IngredientService, RecipeService);
+
+        DailyIntakePage.UpsertDailyIntakeEntryIntoCollection(savedDailyIntakeEntryDto);
+
+        _editingDailyIntakeEntryGuid = Guid.Empty;
+        StateHasChanged();
+    }
+
+    private void HandleAddNewCollection()
+    {
+        if (DailyIntakePage == null)
+        {
+            string errorMessage = "Cannot add new daily intake entry collection because the daily intake page reference has not been set.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        DailyIntakePage.DailyIntakeEntryDtoCollections[Guid.NewGuid()] = new();
+    }
+
+    private void HandleDailyIntakeEntryEditCancel()
+    {
+        _editingDailyIntakeEntryGuid = Guid.Empty;
+        StateHasChanged();
+    }
+
+    private async Task HandleDailyIntakeEntryRemovalConfirmed(DailyIntakeEntryDto removedDailyIntakeEntryDto)
+    {
+        if (DailyIntakePage == null)
+        {
+            string errorMessage = "Cannot remove daily intake entry from daily intake page because daily intake page reference is null.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        if (DailyIntakeService == null)
+        {
+            string errorMessage = "Daily intake service is not available during daily intake entry removal.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        if (ToastService == null)
+        {
+            string errorMessage = "Toast service is not available during daily intake entry removal.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        Guid removedId = removedDailyIntakeEntryDto.Id;
+        Guid removedCollectionId = removedDailyIntakeEntryDto.CollectionId;
+
+        bool removalFromDatabaseSuccessful = await DailyIntakeService.DeleteAsync(removedId);
+        bool removalFromCollectionSuccessful = false;
+
+        foreach (List<DailyIntakeEntryDto> dailyIntakeEntryDtos in DailyIntakePage.DailyIntakeEntryDtoCollections.Values)
+        {
+            DailyIntakeEntryDto? soughtDailyIntakeEntryDto = dailyIntakeEntryDtos.Find(dailyIntakeEntryDto => dailyIntakeEntryDto.Id == removedId);
+
+            if (soughtDailyIntakeEntryDto != null)
+            {
+                dailyIntakeEntryDtos.Remove(soughtDailyIntakeEntryDto);
+
+                if (dailyIntakeEntryDtos.Count == 0)
+                {
+                    DailyIntakePage.DailyIntakeEntryDtoCollections.Remove(removedCollectionId);
+                }
+
+                removalFromCollectionSuccessful = true;
+                break;
+            }
+        }
+
+        if (removalFromDatabaseSuccessful && removalFromCollectionSuccessful)
+        {
+            ToastService.ShowInfo("Post för dagligt intag borttagen.");
+            StateHasChanged();
+        }
+    }
+
+    private string GetAmount(DailyIntakeEntryDto dailyIntakeEntryDto)
+    {
+        if (dailyIntakeEntryDto.IsRecipe)
+        {
+            string portionSuffix = (dailyIntakeEntryDto.Amount > 1) ? "portioner" : "portion";
+
+            return $"{Math.Round(dailyIntakeEntryDto.Amount, 2)} {portionSuffix}";
+        }
+        else
+        {
+            return $"{Math.Round(dailyIntakeEntryDto.Amount, 2)} gram";
+        }
+    }
+
+    private double GetFat(DailyIntakeEntryDto dailyIntakeEntryDto)
+    {
+        return Math.Round(dailyIntakeEntryDto.Fat, 2);
+    }
+
+    private double GetCarbohydrates(DailyIntakeEntryDto dailyIntakeEntryDto)
+    {
+        return Math.Round(dailyIntakeEntryDto.Carbohydrates, 2);
+    }
+
+    private double GetProtein(DailyIntakeEntryDto dailyIntakeEntryDto)
+    {
+        return Math.Round(dailyIntakeEntryDto.Protein, 2);
+    }
+
+    private double GetCalories(DailyIntakeEntryDto dailyIntakeEntryDto)
+    {
+        return Math.Round(dailyIntakeEntryDto.Calories, 2);
+    }
+
+    private double GetProteinPerCalorie(DailyIntakeEntryDto dailyIntakeEntryDto)
+    {
+        return Math.Round(dailyIntakeEntryDto.ProteinPerCalorie, 2);
+    }
+
+    private double GetFatTotal(List<DailyIntakeEntryDto> dailyIntakeEntryDtos)
+    {
+        if (dailyIntakeEntryDtos == null)
+        {
+            string errorMessage = "Cannot present daily intake grams total because daily intake list is not set.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        double fatTotal = dailyIntakeEntryDtos.Sum(dailyIntakeEntryDto => dailyIntakeEntryDto.Fat);
+
+        return Math.Round(fatTotal, 2);
+    }
+
+    private double GetCarbohydrateTotal(List<DailyIntakeEntryDto> dailyIntakeEntryDtos)
+    {
+        if (dailyIntakeEntryDtos == null)
+        {
+            string errorMessage = "Cannot present daily intake carbohydrate total because daily intake list is not set.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        double carbohydrateTotal = dailyIntakeEntryDtos.Sum(dailyIntakeEntryDto => dailyIntakeEntryDto.Carbohydrates);
+
+        return Math.Round(carbohydrateTotal, 2);
+    }
+
+    private double GetProteinTotal(List<DailyIntakeEntryDto> dailyIntakeEntryDtos)
+    {
+        if (dailyIntakeEntryDtos == null)
+        {
+            string errorMessage = "Cannot present daily intake protein total because daily intake list is not set.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        double proteinTotal = dailyIntakeEntryDtos.Sum(dailyIntakeEntryDto => dailyIntakeEntryDto.Protein);
+
+        return Math.Round(proteinTotal, 2);
+    }
+
+    private double GetCalorieTotal(List<DailyIntakeEntryDto> dailyIntakeEntryDtos)
+    {
+        if (dailyIntakeEntryDtos == null)
+        {
+            string errorMessage = "Cannot present daily intake calorie total because daily intake list is not set.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        double calorieTotal = dailyIntakeEntryDtos.Sum(dailyIntakeEntryDto => dailyIntakeEntryDto.Calories);
+
+        return Math.Round(calorieTotal, 2);
+    }
+
+    private double GetAverageProteinPerCalorie(List<DailyIntakeEntryDto> dailyIntakeEntryDtos)
+    {
+        if (dailyIntakeEntryDtos == null)
+        {
+            string errorMessage = "Cannot present daily intake protein per gram because daily intake list is not set.";
+            Log.ForContext(_logProperty, _logDomainName).Error(errorMessage);
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        if (dailyIntakeEntryDtos.Count == 0)
+        {
+            return 0;
+        }
+
+        double proteinPerCalorieSum = dailyIntakeEntryDtos.Sum(dailyIntakeEntryDto => dailyIntakeEntryDto.ProteinPerCalorie);
+        double proteinPerCalorieAverage = proteinPerCalorieSum / dailyIntakeEntryDtos.Count;
+
+        return Math.Round(proteinPerCalorieAverage, 2);
+    }
+}
