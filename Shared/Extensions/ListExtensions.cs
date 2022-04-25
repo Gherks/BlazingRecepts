f4b5d4249acd37f09f91ecb25f18125ba328f6d9
Gherks <nils-joakim.andersson@hotmail.com>
2022-04-25 18:34:12 +0200

    Add list extension method that swaps position of two elements in list

---
+++ Shared/Extensions/ListExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazingRecept.Shared.Extensions;
+
+public static class ListExtensions
+{
+    public static IList<Type> Swap<Type>(this IList<Type> list, int firstIndex, int secondIndex)
+    {
+        Type tmp = list[firstIndex];
+        list[firstIndex] = list[secondIndex];
+        list[secondIndex] = tmp;
+        return list;
+    }
+}
