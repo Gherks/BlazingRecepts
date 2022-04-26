6e34d1dd49a2da2f38fd0d1f0624bf9164adedab
Gherks <nils-joakim.andersson@hotmail.com>
2022-04-26 11:57:47 +0200

    Properly calculate ingredient measurements in ingredient measurement table

---
+++ Client/Extensions/IngredientMeasurementDtoExtensions.cs
@@ -0,0 +1,34 @@
+﻿using BlazingRecept.Shared.Dto;
+
+namespace BlazingRecept.Client.Extensions;
+
+public static class IngredientMeasurementDtoExtensions
+{
+    public static double GetFat(this IngredientMeasurementDto ingredientMeasurementDto)
+    {
+        double fat = ingredientMeasurementDto.IngredientDto.Fat * ingredientMeasurementDto.Grams * 0.01f;
+
+        return Math.Round(fat, 2);
+    }
+
+    public static double GetCarbohydrates(this IngredientMeasurementDto ingredientMeasurementDto)
+    {
+        double carbohydrates = ingredientMeasurementDto.IngredientDto.Carbohydrates * ingredientMeasurementDto.Grams * 0.01f;
+
+        return Math.Round(carbohydrates, 2);
+    }
+
+    public static double GetProtein(this IngredientMeasurementDto ingredientMeasurementDto)
+    {
+        double protein = ingredientMeasurementDto.IngredientDto.Protein * ingredientMeasurementDto.Grams * 0.01f;
+
+        return Math.Round(protein, 2);
+    }
+
+    public static double GetCalories(this IngredientMeasurementDto ingredientMeasurementDto)
+    {
+        double calories = ingredientMeasurementDto.IngredientDto.Calories * ingredientMeasurementDto.Grams * 0.01f;
+
+        return Math.Round(calories, 2);
+    }
+}
