66adfe210815701f48781cd75a8a416e2bd42587
Gherks <nils-joakim.andersson@hotmail.com>
2022-04-26 12:03:44 +0200

    Clean up

--- Client/Components/Utilities/ProcessingButton.razor.cs
+++ Client/Components/Utilities/ProcessingButton.razor.cs
@@ -6,7 +6,7 @@ public partial class ProcessingButton : ComponentBase
 {
     private bool _isProcessing = false;
 
-    public bool IsProcessing 
+    public bool IsProcessing
     {
         get
         {
