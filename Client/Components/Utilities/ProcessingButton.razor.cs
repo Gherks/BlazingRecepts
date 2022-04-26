c2fc5ec9e60e54a7b4c60c9fbbc0835ed68f6f8f
Gherks <nils-joakim.andersson@hotmail.com>
2022-04-26 11:58:07 +0200

    Add processing button component

---
+++ Client/Components/Utilities/ProcessingButton.razor.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Components;
+
+namespace BlazingRecept.Client.Components.Utilities;
+
+public partial class ProcessingButton : ComponentBase
+{
+    private bool _isProcessing = false;
+
+    public bool IsProcessing 
+    {
+        get
+        {
+            return _isProcessing;
+        }
+
+        set
+        {
+            _isProcessing = value;
+            StateHasChanged();
+        }
+    }
+
+    [Parameter]
+    public string Label { get; set; } = string.Empty;
+
+    [Parameter]
+    public string EditFormId { get; set; } = string.Empty;
+
+    [Parameter]
+    public string CssClass { get; set; } = "btn btn-primary";
+
+    [Parameter]
+    public string Type { get; set; } = "submit";
+}
