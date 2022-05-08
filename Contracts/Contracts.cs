a017c54f38779e56204b1b377fa4fd91a0bbe9aa
Gherks <nils-joakim.andersson@hotmail.com>
2022-05-08 16:51:31 +0200

    Move Contracts object into its own library
    This is done so that logging has an easier time with detecting which call caused the log

---
+++ Contracts/Contracts.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace BlazingRecept.Shared;
+
+public static class Contracts
+{
+    public static void LogAndThrowWhenNull([NotNull] object? value, string errorMessage)
+    {
+        if (value is null)
+        {
+            Log.Error(errorMessage);
+            throw new ArgumentNullException(nameof(value), errorMessage);
+        }
+    }
+}
